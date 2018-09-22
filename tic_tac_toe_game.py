5dc1177fc42bd3c4aa83013db2335d8dfb37c418
Joel Sotelo <joel.pilgrim88@gmail.com>
2018-09-22 16:40:23 -0500

    instructions and some text printing 2

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,1 +1,36 @@
-empty
+
+#2 players
+#the board should be printed out every time a player makes a move.
+#You should be able to accept input of the player position and then place a symbol on the board.
+
+print('Welcome to the real world!')
+print('Player 1: Do you wanna be X or O?')
+
+
+playerone = input().upper()
+
+if playerone == 'X' or playerone == 'O':
+    print('Player 1 will go first.')
+else:
+    print('WFT')
+
+print('Are you ready to play? Enter Yes or No.')
+
+print('Choose your next position: (1-9)')
+
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('-----------------')
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('-----------------')
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+print('  {}  |  {}  |  {}  '.format('X','Y','Z'))
+
+
+print('Congratulations! You have won the game!')
+
+print('Do you wanna play again? Enter Yes or No:')
