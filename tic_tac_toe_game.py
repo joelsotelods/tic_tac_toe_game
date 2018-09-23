02c6c5cb2d8a5b3fe18c61f7879d93fd6c08b5c2
Joel Sotelo <joel.pilgrim88@gmail.com>
2018-09-23 01:24:02 -0500

    Last adjustments for today lol

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,7 +1,4 @@
 
-#2 players
-#the board should be printed out every time a player makes a move.
-#You should be able to accept input of the player position and then place a symbol on the board.
 
 # from IPython.display import clear_output
 
@@ -11,11 +8,9 @@ def getplayers():
 
     while True: 
             
-        print('Player 1: Do you wanna be X or O?')
-
         players_xd = ['X','O']
 
-        players_xd[0] = input().upper()
+        players_xd[0] = input('Player 1: Do you wanna be X or O?').upper()
 
         if players_xd[0] == 'X':
             print('Player 1 will go first with {}.'.format(players_xd[0]))
@@ -50,13 +45,34 @@ def printmatrix (myshots):
     print('     |     |     ')
 
 
+def win_check(myshots,turns):
+    
+    
+    if (myshots[0] == myshots[1] == myshots[2] and myshots[1] != ' ') or (myshots[3] == myshots[4] == myshots[5] and myshots[4] != ' ') or (myshots[6] == myshots[7] == myshots[8] and myshots[7] != ' ') or (myshots[0] == myshots[3] == myshots[6] and myshots[6] != ' ') or (myshots[1] == myshots[4] == myshots[7] and myshots[4] != ' ') or (myshots[2] == myshots[5] == myshots[8] and myshots[5] != ' ') or (myshots[0] == myshots[4] == myshots[8] and myshots[4] != ' ') or (myshots[2] == myshots[4] == myshots[6] and myshots[4] != ' '):
+            
+        printmatrix(myshots)
+
+        if turns == 0:
+            print('Congratulations Player 2! You have won the game!\n\n')
+        else:
+            print('Congratulations Player 1! You have won the game!\n\n')
+            
+        return True
+    
+    elif ' ' not in myshots:
+        printmatrix(myshots)
+        print('\nThe game was Even!')
+        return True
+        
+
+
 
 def playnow(players):
 
     turns = 0
-    myshots = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    shotss = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
     
-    printmatrix(myshots)
+    printmatrix(shotss)
 
     while True:
 
@@ -64,39 +80,23 @@ def playnow(players):
 
         place = int(input())
 
-        if place in range(1,10) and myshots[place-1] == ' ':
-            myshots[place-1] = players[turns]
+        if place in range(1,10) and shotss[place-1] == ' ':
+            shotss[place-1] = players[turns]
             
-            printmatrix(myshots)
+            printmatrix(shotss)
 
             if turns == 0:
                 turns = 1
             else:
                 turns = 0
             
-        elif myshots[place-1] != ' ':
+        elif shotss[place-1] != ' ':
             print('Motherfucker.. that cell is used')
         else:
             print('Only numbers between 1-9!!')
         
 
-
-        if (myshots[0] == myshots[1] and myshots[1] == myshots[2] and myshots[1] != ' ') or (myshots[3] == myshots[4] and myshots[4] == myshots[5] and myshots[4] != ' ') or (myshots[6] == myshots[7] and myshots[7] == myshots[8] and myshots[7] != ' ') or (myshots[0] == myshots[3] and myshots[3] == myshots[6] and myshots[6] != ' ') or (myshots[1] == myshots[4] and myshots[4] == myshots[7] and myshots[4] != ' ') or (myshots[2] == myshots[5] and myshots[5] == myshots[8] and myshots[5] != ' ')  or (myshots[0] == myshots[4] and myshots[4] == myshots[8] and myshots[4] != ' '):
-            
-            printmatrix(myshots)
-            if turns == 0:
-
-                print('Congratulations Player 2! You have won the game!\n\n')
-            else:
-
-                print('Congratulations Player 1! You have won the game!\n\n')
-            
-            break
-        elif myshots[0] != ' ' and myshots[1] != ' ' and myshots[2] != ' ' and myshots[3] != ' ' and myshots[4] != ' ' and myshots[5] != ' ' and myshots[6] != ' ' and myshots[7] != ' ' and myshots[8] != ' ':
-
-            printmatrix(myshots)
-            
-            print('\nThe game was Even!')
+        if win_check(shotss,turns):
             break
 
 
@@ -112,24 +112,21 @@ players_chars = getplayers()
 input('Are you ready to play? Press Enter.').upper()
 
 
+rplayagain = 'x'
 
-
-while True:
+while rplayagain != 'no':
     
     playnow(players_chars)
-    
+
     rplayagain = 'x'
 
     while rplayagain != 'yes' and rplayagain != 'no':
 
         rplayagain = input('Do you wanna play again? Enter Yes or No:').lower()
 
-        if rplayagain == 'no':
-            print('\nBye!')
-            break
-    
-        elif rplayagain != 'yes':
+        if rplayagain != 'yes' and rplayagain != 'no':
             print('\nPlease only Yes or No')
-            continue
+    
+        
 
-
+print('\nBye!')
