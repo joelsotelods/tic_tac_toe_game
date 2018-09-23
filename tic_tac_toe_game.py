6bf3d36510f445fb45a85a427fbfc9c6fc8dde9f
Joel Sotelo <joel.pilgrim88@gmail.com>
2018-09-23 14:21:40 -0500

    Now the program asks for Player Names and keeps a Score for the current game

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -4,20 +4,32 @@
 
 import os
 
-def getplayers():
+def playernames():
+    
+    playernames = {'player1':'none1', 'player1score':0, 'player2':'none2', 'player2score':0, }
+
+    playernames['player1'] = input('\nPlayer 1: what is your name son?: ').capitalize()
+    playernames['player2'] = input('\nPlayer 2: what is your name son?: ').capitalize()
+
+    return playernames
+
+
+def getplayers(playernames):
+
+    players_xd = ['X','O']
 
     while True: 
             
-        players_xd = ['X','O']
+        
 
-        players_xd[0] = input('Player 1: Do you wanna be X or O?').upper()
+        players_xd[0] = input('\n{}: Do you wanna be X or O?: '.format(playernames['player1'])).upper()
 
         if players_xd[0] == 'X':
-            print('Player 1 will go first with {}.'.format(players_xd[0]))
+            print('\nReady, {} will go first with {}.'.format(playernames['player1'],players_xd[0]))
             break
         elif players_xd[0] == 'O':
             players_xd[1] = 'X'
-            print('Player 1 will go first with {}.'.format(players_xd[0]))
+            print('\nReady, {} will go first with {}.'.format(playernames['player1'],players_xd[0]))
             break
         else:
             print('WFT X = equis ; O = es una o no cero')
@@ -45,17 +57,26 @@ def printmatrix (myshots):
     print('     |     |     ')
 
 
-def win_check(myshots,turns):
+def win_check(myshots,turns,playernames):
     
     
-    if (myshots[0] == myshots[1] == myshots[2] and myshots[1] != ' ') or (myshots[3] == myshots[4] == myshots[5] and myshots[4] != ' ') or (myshots[6] == myshots[7] == myshots[8] and myshots[7] != ' ') or (myshots[0] == myshots[3] == myshots[6] and myshots[6] != ' ') or (myshots[1] == myshots[4] == myshots[7] and myshots[4] != ' ') or (myshots[2] == myshots[5] == myshots[8] and myshots[5] != ' ') or (myshots[0] == myshots[4] == myshots[8] and myshots[4] != ' ') or (myshots[2] == myshots[4] == myshots[6] and myshots[4] != ' '):
+    if (myshots[0] == myshots[1] == myshots[2] and myshots[1] != ' ') or \
+        (myshots[3] == myshots[4] == myshots[5] and myshots[4] != ' ') or \
+        (myshots[6] == myshots[7] == myshots[8] and myshots[7] != ' ') or \
+        (myshots[0] == myshots[3] == myshots[6] and myshots[6] != ' ') or \
+        (myshots[1] == myshots[4] == myshots[7] and myshots[4] != ' ') or \
+        (myshots[2] == myshots[5] == myshots[8] and myshots[5] != ' ') or \
+        (myshots[0] == myshots[4] == myshots[8] and myshots[4] != ' ') or \
+        (myshots[2] == myshots[4] == myshots[6] and myshots[4] != ' '):
             
         printmatrix(myshots)
 
         if turns == 0:
-            print('Congratulations Player 2! You have won the game!\n\n')
+            print('Congratulations {}! You have won the game!\n\n'.format(playernames['player2']))
+            playernames['player2score'] += 1
         else:
-            print('Congratulations Player 1! You have won the game!\n\n')
+            print('Congratulations {}! You have won the game!\n\n'.format(playernames['player1']))
+            playernames['player1score'] += 1
             
         return True
     
@@ -67,7 +88,7 @@ def win_check(myshots,turns):
 
 
 
-def playnow(players):
+def playnow(players,player_names):
 
     turns = 0
     shotss = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
@@ -76,11 +97,25 @@ def playnow(players):
 
     while True:
 
-        print('\nPlayer #{} Choose your next position: (1-9)'.format(turns+1))
+        if (turns == 0):
+            print('\n{} Choose your next position: (1-9)'.format(player_names['player1']))
+        else:
+            print('\n{} Choose your next position: (1-9)'.format(player_names['player2']))
+        
+        place = 0
+        
+        while True:
+            try:
+                place = int(input())   
+            except ValueError:
+                print('Only numbers between 1-9!!')
+            else:
+                if place in range(1,10):
+                    break 
+                else:
+                    print('Only numbers between 1-9!!')
 
-        place = int(input())
-
-        if place in range(1,10) and shotss[place-1] == ' ':
+        if shotss[place-1] == ' ':
             shotss[place-1] = players[turns]
             
             printmatrix(shotss)
@@ -89,14 +124,13 @@ def playnow(players):
                 turns = 1
             else:
                 turns = 0
-            
-        elif shotss[place-1] != ' ':
-            print('Motherfucker.. that cell is used')
+
         else:
-            print('Only numbers between 1-9!!')
+            print('Oliii!.. that cell is used')
+
         
 
-        if win_check(shotss,turns):
+        if win_check(shotss,turns,player_names):
             break
 
 
@@ -105,22 +139,30 @@ def playnow(players):
 os.system('clear')
 #clear_output()
     
-print('Welcome to the real world!')
+print('Neo... welcome to the real world.')
 
-players_chars = getplayers()
+player_names = playernames()
 
-input('Are you ready to play? Press Enter.').upper()
+players_chars = getplayers(player_names)
+
+input('\nAre you ready to play?\nPress Enter.').upper()
 
 
 rplayagain = 'x'
 
 while rplayagain != 'no':
     
-    playnow(players_chars)
+    playnow(players_chars,player_names)
 
     rplayagain = 'x'
 
+    print('Scores:')
+    print('{}: {}'.format(player_names['player1'],player_names['player1score']))
+    print('{}: {}\n'.format(player_names['player2'],player_names['player2score']))
+
     while rplayagain != 'yes' and rplayagain != 'no':
+
+
 
         rplayagain = input('Do you wanna play again? Enter Yes or No:').lower()
 
